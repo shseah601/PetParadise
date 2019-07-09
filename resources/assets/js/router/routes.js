2eb5300272e658b0092911b28e89b40e0bd91c85
shseah601 <shseah601@gmail.com>
2019-07-10 01:25:53 +0800

    created components and add routes for admin panel

--- resources/assets/js/router/routes.js
+++ resources/assets/js/router/routes.js
@@ -4,13 +4,36 @@ export default ({ authGuard, guestGuard }) => [
   // Authenticated routes.
   ...authGuard([
     { path: '/home', name: 'home', component: require('~/pages/home.vue') },
-    { path: '/settings',
+    { path: '/dashboard', name: 'dashboard', component: require('~/pages/dashboard.vue') },
+    { path: '/calendar', name: 'calendar', component: require('~/pages/calendar.vue') },
+    { path: '/services', name: 'services', component: require('~/pages/services.vue') },
+    { path: '/reports', name: 'reports', component: require('~/pages/reports.vue') },
+    {
+      path: '/user',
+      component: require('~/pages/user/index.vue'),
+      children: [
+        { path: '/client', name: 'user.client', component: require('~/pages/user/client.vue') },
+        { path: '/employee', name: 'user.employee', component: require('~/pages/user/employee.vue') }
+      ]
+    },
+    {
+      path: '/company',
+      component: require('~/pages/company/index.vue'),
+      children: [
+        { path: '', redirect: { name: 'company.detail' } },
+        { path: '/detial', name: 'company.detail', component: require('~/pages/company/detail.vue') },
+        { path: '/workinghour', name: 'company.workinghour', component: require('~/pages/company/workinghour.vue') }
+      ]
+    },
+    {
+      path: '/settings',
       component: require('~/pages/settings/index.vue'),
       children: [
-      { path: '', redirect: { name: 'settings.profile' } },
-      { path: 'profile', name: 'settings.profile', component: require('~/pages/settings/profile.vue') },
-      { path: 'password', name: 'settings.password', component: require('~/pages/settings/password.vue') }
-      ] }
+        { path: '', redirect: { name: 'settings.profile' } },
+        { path: 'profile', name: 'settings.profile', component: require('~/pages/settings/profile.vue') },
+        { path: 'password', name: 'settings.password', component: require('~/pages/settings/password.vue') }
+      ]
+    }
   ]),
 
   // Guest routes.
